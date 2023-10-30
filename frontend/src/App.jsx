8c53a3d960ab9d204198095d897b0d2e17624f52
tshepoandi <lovekilledtshepo@gmail.com>
2023-10-30 10:22:26 +0200

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
 import './App.css';
 import Navbar from './Components/NavBar/Navbar';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignUp from './Pages/LoginSignUp';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
+      </BrowserRouter>
+        <Routes>
+          <Route path="/" element={Shop}/>
+          <Route path="/mens" element={<ShopCategory category="men"/>}/>
+          <Route path="/womens" element={<ShopCategory category="womens"/>}/>
+          <Route path="/kids" element={<ShopCategory category="kids"/>}/>
+          <Route path="/product" elemen={<Product/>}>
+            <Route path=":productId" element={<Product/>}/>
+          </Route>  
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/login" element={<LoginSignUp/>}/>
+        </Routes>
+      
     </div>
   );
 }
---
+++ frontend/src/Pages/LoginSignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginSignUp() {
+  return (
+    <div>LoginSignUp</div>
+  )
+}
+
+export default LoginSignUp
