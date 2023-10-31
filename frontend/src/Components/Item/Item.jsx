8401b8af30e7d889846d753dfc9878688e91a0a7
tshepoandi <lovekilledtshepo@gmail.com>
2023-10-31 12:00:50 +0200

    login

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -3,7 +3,7 @@ import './Item.css'
 const Item = (props) => {
   return (
     <div className='item'>
-      <img src={props.image} alt=''/>
+      <img className='picture' src={props.image} alt=''/>
       <p>{props.name}</p>
       <div className='item-prices'>
         <div className='item-price-new'>
--- frontend/src/Pages/LoginSignUp.jsx
+++ frontend/src/Pages/LoginSignUp.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import "../css/loginignup.css"
 function LoginSignUp() {
   return (
-    <div>LoginSignUp</div>
+    <div className='loginsign'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="text" placeholder='Email Address' />
+          <input type="text"  placeholder='Password'/>
+        </div>
+        <button>Continue</button>
+        <p className='loginignup-login'>Already have an account?<span> Login</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id=''/>
+          <p>By Continuing, I agree to the terms of use and orivacy policy</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
