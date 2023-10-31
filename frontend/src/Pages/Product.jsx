6546a48a82048bbec1c85ba5be75698ffc560485
tshepoandi <lovekilledtshepo@gmail.com>
2023-10-31 13:42:44 +0200

    breadcrums

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,8 +1,16 @@
- import React from 'react'
+ import React, { useContext } from 'react'
+import ShopCategory from './ShopCategory'
+import { useParams } from 'react-router-dom'
+import Breadcrums from '../Components/Breadcrums/Breadcrums'
  
  function Product() {
+  const {all_product} = useContext(ShopCategory)
+  const {productId} = useParams()
+  const product = all_product.find((e)=> e.id === Number(productId))
    return (
-     <div>Product</div>
+     <div>
+        <Breadcrums product={product}/>
+     </div>
    )
  }
  
